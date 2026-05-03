92500af8d49175eee496729282a96dd03afa117b
MaxZ <bigztbmstu@ya.ru>
2026-05-03 14:22:20 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/SPAReactFrontend/sw.js', { scope: '/SPAReactFrontend/' })})}
